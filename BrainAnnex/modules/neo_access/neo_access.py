84fa6b0e334a9ac2dc648d251c6c9f4dc46efb33
Julian West <julian.x.west@gsk.com>
2021-12-15 03:00:01 -0800

    bugfix, set defaults, and test export_nodes_rels_json()

--- BrainAnnex/modules/neo_access/neo_access.py
+++ BrainAnnex/modules/neo_access/neo_access.py
@@ -2072,7 +2072,7 @@ class NeoAccess:
                     as well as a "data" field with the actual export as a JSON string
         """
         cypher = '''
-            CALL apoc.export.json.all(null, {useTypes: true, stream: true})
+            CALL apoc.export.json.all(null, {useTypes: true, stream: true, jsonFormat: "JSON_LINES"})
             YIELD nodes, relationships, properties, data
             RETURN nodes, relationships, properties, data
             '''
@@ -2082,28 +2082,34 @@ class NeoAccess:
         export_dict = result[0]         # This will be a dictionary with 4 keys: "nodes", "relationships", "properties", "data"
                                         #   "nodes", "relationships" and "properties" contain their respective counts
 
-        pseudo_json = export_dict["data"]
-        # Who knows why, the string returned by the APOC function isn't actual JSON! :o  Some tweaking needed to produce valid JSON...
-        # TODO: it seems related to the default format of "JSON_LINES".  See https://neo4j.com/labs/apoc/4.3/export/json/#export-nodes-relationships-json
-        json_str = "[" + pseudo_json.replace("\n", ",\n ") + "\n]"  # The newlines \n make the JSON much more human-readable
-        export_dict["data"] = json_str
-        #print(export_dict)
+        json_lines = export_dict["data"]
+        # Tweak the "JSON_LINES" format to make it a valid JSON string, and more readable.
+        # See https://neo4j.com/labs/apoc/4.3/export/json/#export-nodes-relationships-json
+        json_str = "[" + json_lines.replace("\n", ",\n ") + "\n]"   # The newlines \n make the JSON much more human-readable
+        export_dict["data"] = json_str                              # Replace the "data" value
+
+        #print("export_dict = ", export_dict)
 
         return export_dict
 
 
-    def export_nodes_rels_json(self, nodes_query: str, rels_query: str) -> {}:      # TODO: test
+
+    def export_nodes_rels_json(self, nodes_query="", rels_query="") -> {}:
         """
         Export the specified nodes, plus the specified relationships, as a JSON string.
+        The default empty strings are taken to mean (respectively) ALL nodes/relationships.
+
         For details on the formats, see export_dbase_json()
 
         IMPORTANT: APOC must be activated in the database for this function
 
         :param nodes_query: A Cypher query to identify the desired nodes (exclusive of RETURN statements)
                                     The dummy variable for the nodes must be "n"
+                                    Use "" to request all nodes
                                     EXAMPLE: "MATCH (n) WHERE (n:CLASS OR n:PROPERTY)"
         :param rels_query:   A Cypher query to identify the desired relationships (exclusive of RETURN statements)
                                     The dummy variable for the relationships must be "r"
+                                    Use "" to request all relationships (whether or not their end nodes are also exported)
                                     EXAMPLE: "MATCH ()-[r:HAS_PROPERTY]->()"
 
         :return:    A dictionary specifying the number of nodes exported,
@@ -2111,37 +2117,48 @@ class NeoAccess:
                     as well as a "data" field with the actual export as a JSON string
 
         """
+        if nodes_query == "":
+            nodes_query = "MATCH (n)"           # All nodes by default
+
+        if rels_query == "":
+            rels_query = "MATCH ()-[r]->()"     # All relationships by default
+
         cypher = f'''
             {nodes_query}
             WITH collect(n) as nds
-            {rels_query}
+            OPTIONAL {rels_query}
             WITH nds, collect(r) AS rels
-            CALL apoc.export.json.data(nds, rels, null, {{stream: true}})
+            CALL apoc.export.json.data(nds, rels, null, {{stream: true, jsonFormat: "JSON_LINES"}})
             YIELD nodes, relationships, properties, data
             RETURN nodes, relationships, properties, data
             '''
+        # The "OPTIONAL" keyword is necessary in cases where there are no relationships
 
         # Example of complete Cypher query
         '''
             MATCH (s) WHERE (s:CLASS OR s:PROPERTY) 
             WITH collect(s) as nds
-            MATCH ()-[r:HAS_PROPERTY]->()
+            OPTIONAL MATCH ()-[r:HAS_PROPERTY]->()
             WITH nds, collect(r) AS rels 
-            CALL apoc.export.json.data(nds, rels, null, {stream: true})
+            CALL apoc.export.json.data(nds, rels, null, {stream: true, jsonFormat: "ARRAY_JSON"})
             YIELD nodes, relationships, properties, data
             RETURN nodes, relationships, properties, data
         '''
-        result = self.query(cypher)     # It returns a list with a single element
-        export_dict = result[0]         # This will be a dictionary with 4 keys: "nodes", "relationships", "properties", "data"
-                                        #   "nodes", "relationships" and "properties" contain their respective counts
 
-        pseudo_json = export_dict["data"]
-        # Who knows why, the string returned by the APOC function isn't actual JSON! :o  Some tweaking needed to produce valid JSON...
-        # TODO: it seems related to the default format of "JSON_LINES".  See https://neo4j.com/labs/apoc/4.3/export/json/#export-nodes-relationships-json
-        # TODO: in the cypher query above, try replacing {stream: true} with {stream: true, jsonFormat: ARRAY_JSON}
-        json_str = "[" + pseudo_json.replace("\n", ",\n ") + "\n]"  # The newlines \n make the JSON much more human-readable
-        export_dict["data"] = json_str
-        #print(export_dict)
+        #self.debug_print(cypher, method="export_nodes_rels_json", force_output=True)
+        result = self.query(cypher)     # It returns a list with a single element
+        #print("In export_nodes_rels_json(): result = ", result)
+
+        export_dict = result[0]     # This will be a dictionary with 4 keys: "nodes", "relationships", "properties", "data"
+                                    #   "nodes", "relationships" and "properties" contain their respective counts
+
+        json_lines = export_dict["data"]
+        # Tweak the "JSON_LINES" format to make it a valid JSON string, and more readable.
+        # See https://neo4j.com/labs/apoc/4.3/export/json/#export-nodes-relationships-json
+        json_str = "[" + json_lines.replace("\n", ",\n ") + "\n]"       # The newlines \n make the JSON much more human-readable
+        export_dict["data"] = json_str                                  # Replace the "data" value
+
+        #print("export_dict = ", export_dict)
 
         return export_dict
 
--- tests/test_neoaccess_json.py
+++ tests/test_neoaccess_json.py
@@ -21,13 +21,13 @@ def test_export_dbase_json(db):
     result = db.export_dbase_json()
     assert result == {'nodes': 0, 'relationships': 0, 'properties': 0, 'data': '[\n]'}
 
-    # Create a first node
-    node_id1 = db.create_node("User", {'name': 'Eve'})
+    # Create a first node ("Eve")
+    node_id_eve = db.create_node("User", {'name': 'Eve'})
     result = db.export_dbase_json()
     assert result['nodes'] == 1
     assert result['relationships'] == 0
     assert result['properties'] == 1
-    expected_json = '[{"type":"node","id":"' + str(node_id1) + '","labels":["User"],"properties":{"name":"Eve"}}\n]'
+    expected_json = '[{"type":"node","id":"' + str(node_id_eve) + '","labels":["User"],"properties":{"name":"Eve"}}\n]'
     assert result['data'] == expected_json
     ''' EXAMPLE of JSON string:
                 [{"type":"node","id":"100","labels":["User"],"properties":{"name":"Eve"}}
@@ -35,13 +35,13 @@ def test_export_dbase_json(db):
     '''
 
 
-    # Create a 2nd node
-    node_id2 = db.create_node("User", {'name': 'Adam', 'age': 30})
+    # Create a 2nd node ("Adam")
+    node_id_adam = db.create_node("User", {'name': 'Adam', 'age': 30})
     result = db.export_dbase_json()
     assert result['nodes'] == 2
     assert result['relationships'] == 0
     assert result['properties'] == 3
-    expected_json = f'[{{"type":"node","id":"{node_id1}","labels":["User"],"properties":{{"name":"Eve"}}}},\n {{"type":"node","id":"{node_id2}","labels":["User"],"properties":{{"name":"Adam","age":30}}}}\n]'
+    expected_json = f'[{{"type":"node","id":"{node_id_eve}","labels":["User"],"properties":{{"name":"Eve"}}}},\n {{"type":"node","id":"{node_id_adam}","labels":["User"],"properties":{{"name":"Adam","age":30}}}}\n]'
     assert result['data'] == expected_json
     ''' EXAMPLE of JSON string:
                 [{"type":"node","id":"100","labels":["User"],"properties":{"name":"Eve"}},
@@ -50,7 +50,7 @@ def test_export_dbase_json(db):
     '''
 
     # Now add relationship (with no properties) between the above two nodes
-    db.link_nodes_by_ids(node_id1, node_id2, "LOVES")
+    db.link_nodes_by_ids(node_id_eve, node_id_adam, "LOVES")
     # Look up the Neo4j ID of the relationship just created
     cypher = "MATCH (to)-[r]->(from) RETURN id(r) AS rel_id"
     query_result = db.query(cypher)
@@ -60,9 +60,9 @@ def test_export_dbase_json(db):
     assert result['nodes'] == 2
     assert result['relationships'] == 1
     assert result['properties'] == 3
-    expected_json = f'[{{"type":"node","id":"{node_id1}","labels":["User"],"properties":{{"name":"Eve"}}}},\n' \
-                    f' {{"type":"node","id":"{node_id2}","labels":["User"],"properties":{{"name":"Adam","age":30}}}},\n' \
-                    f' {{"id":"{rel_id_1}","type":"relationship","label":"LOVES","start":{{"id":"{node_id1}","labels":["User"]}},"end":{{"id":"{node_id2}","labels":["User"]}}}}\n]'
+    expected_json = f'[{{"type":"node","id":"{node_id_eve}","labels":["User"],"properties":{{"name":"Eve"}}}},\n' \
+                    f' {{"type":"node","id":"{node_id_adam}","labels":["User"],"properties":{{"name":"Adam","age":30}}}},\n' \
+                    f' {{"id":"{rel_id_1}","type":"relationship","label":"LOVES","start":{{"id":"{node_id_eve}","labels":["User"]}},"end":{{"id":"{node_id_adam}","labels":["User"]}}}}\n]'
     assert result['data'] == expected_json
     ''' EXAMPLE of JSON string:
         [{"type":"node","id":"108","labels":["User"],"properties":{"name":"Eve"}},
@@ -72,7 +72,7 @@ def test_export_dbase_json(db):
     '''
 
     # Add a 2nd relationship (this time with properties) between the two nodes
-    db.link_nodes_by_ids(node_id1, node_id2, "KNOWS", {'since': 1976, 'intensity': 'eternal'})
+    db.link_nodes_by_ids(node_id_eve, node_id_adam, "KNOWS", {'since': 1976, 'intensity': 'eternal'})
     # Look up the Neo4j ID of the relationship just created
     cypher = "MATCH (to)-[r:KNOWS]->(from) RETURN id(r) AS rel_id"
     query_result = db.query(cypher)
@@ -82,11 +82,10 @@ def test_export_dbase_json(db):
     assert result['nodes'] == 2
     assert result['relationships'] == 2
     assert result['properties'] == 5    # Note that the 2 properties in the latest relationship went into the count
-    expected_json = f'[{{"type":"node","id":"{node_id1}","labels":["User"],"properties":{{"name":"Eve"}}}},\n' \
-                    f' {{"type":"node","id":"{node_id2}","labels":["User"],"properties":{{"name":"Adam","age":30}}}},\n' \
-                    f' {{"id":"{rel_id_1}","type":"relationship","label":"LOVES","start":{{"id":"{node_id1}","labels":["User"]}},"end":{{"id":"{node_id2}","labels":["User"]}}}},\n' \
-                    f' {{"id":"{rel_id_2}","type":"relationship","label":"KNOWS","properties":{{"intensity":"eternal","since":1976}},"start":{{"id":"{node_id1}","labels":["User"]}},"end":{{"id":"{node_id2}","labels":["User"]}}}}\n]'
-    assert result['data'] == expected_json
+    expected_json = f'[{{"type":"node","id":"{node_id_eve}","labels":["User"],"properties":{{"name":"Eve"}}}},\n' \
+                    f' {{"type":"node","id":"{node_id_adam}","labels":["User"],"properties":{{"name":"Adam","age":30}}}},\n' \
+                    f' {{"id":"{rel_id_1}","type":"relationship","label":"LOVES","start":{{"id":"{node_id_eve}","labels":["User"]}},"end":{{"id":"{node_id_adam}","labels":["User"]}}}},\n' \
+                    f' {{"id":"{rel_id_2}","type":"relationship","label":"KNOWS","properties":{{"intensity":"eternal","since":1976}},"start":{{"id":"{node_id_eve}","labels":["User"]}},"end":{{"id":"{node_id_adam}","labels":["User"]}}}}\n]'
     ''' EXAMPLE of JSON string:
         [{"type":"node","id":"124","labels":["User"],"properties":{"name":"Eve"}},
          {"type":"node","id":"125","labels":["User"],"properties":{"name":"Adam","age":30}},
@@ -94,6 +93,188 @@ def test_export_dbase_json(db):
          {"id":"12","type":"relationship","label":"KNOWS","properties":{"intensity":"eternal","since":1976},"start":{"id":"124","labels":["User"]},"end":{"id":"125","labels":["User"]}}
         ]
     '''
+
+    # The order of the relationships appears to vary randomly: also cover the reverse the lines for "LOVES" and "KNOWS"
+    # (TODO: it may be better to first parse the JSON string into a python entity and then assert)
+    expected_json_reversed_order = f'[{{"type":"node","id":"{node_id_eve}","labels":["User"],"properties":{{"name":"Eve"}}}},\n' \
+                    f' {{"type":"node","id":"{node_id_adam}","labels":["User"],"properties":{{"name":"Adam","age":30}}}},\n' \
+                    f' {{"id":"{rel_id_2}","type":"relationship","label":"KNOWS","properties":{{"intensity":"eternal","since":1976}},"start":{{"id":"{node_id_eve}","labels":["User"]}},"end":{{"id":"{node_id_adam}","labels":["User"]}}}},\n' \
+                    f' {{"id":"{rel_id_1}","type":"relationship","label":"LOVES","start":{{"id":"{node_id_eve}","labels":["User"]}},"end":{{"id":"{node_id_adam}","labels":["User"]}}}}\n]'
+
+    assert (result['data'] == expected_json) or (result['data'] == expected_json_reversed_order)
+
+
+
+def test_export_nodes_rels_json(db):
+    # Completely clear the database
+    db.empty_dbase()
+
+    # Start by exporting everything in the empty database
+    result = db.export_nodes_rels_json(nodes_query = "MATCH (n)", rels_query = "MATCH ()-[r]->()")
+    assert result == {'nodes': 0, 'relationships': 0, 'properties': 0, 'data': '[\n]'}
+
+    # Create a first node ("Eve")
+    node_id_eve = db.create_node("User", {'name': 'Eve'})
+    result = db.export_nodes_rels_json()    # By default, all nodes and all relationships
+    assert result['nodes'] == 1
+    assert result['relationships'] == 0
+    assert result['properties'] == 1
+    expected_json = '[{"type":"node","id":"' + str(node_id_eve) + '","labels":["User"],"properties":{"name":"Eve"}}\n]'
+    assert result['data'] == expected_json
+    ''' EXAMPLE of JSON string:
+                [{"type":"node","id":"100","labels":["User"],"properties":{"name":"Eve"}}
+                ]
+    '''
+
+    # Overlook the one existing node
+    result = db.export_nodes_rels_json(nodes_query = "MATCH (n :NON_EXISTENT_LABEL)")    # No nodes will be found
+    assert result == {'nodes': 0, 'relationships': 0, 'properties': 0, 'data': '[\n]'}
+
+
+    # Create a 2nd node ("Adam")
+    node_id_adam = db.create_node("User", {'name': 'Adam', 'age': 30})
+    result = db.export_nodes_rels_json()    # By default, all nodes and all relationships
+    assert result['nodes'] == 2
+    assert result['relationships'] == 0
+    assert result['properties'] == 3
+    expected_json = f'[{{"type":"node","id":"{node_id_eve}","labels":["User"],"properties":{{"name":"Eve"}}}},\n {{"type":"node","id":"{node_id_adam}","labels":["User"],"properties":{{"name":"Adam","age":30}}}}\n]'
+    assert result['data'] == expected_json
+    ''' EXAMPLE of JSON string:
+                [{"type":"node","id":"100","labels":["User"],"properties":{"name":"Eve"}},
+                 {"type":"node","id":"101","labels":["User"],"properties":{"name":"Adam","age":30}}
+                ]
+    '''
+
+    # Overlook the "Adam" entry, and just pick up "Eve"
+    result = db.export_nodes_rels_json(nodes_query = "MATCH (n) WHERE n.name = 'Eve'")
+    assert result['nodes'] == 1
+    assert result['relationships'] == 0
+    assert result['properties'] == 1
+    expected_json = '[{"type":"node","id":"' + str(node_id_eve) + '","labels":["User"],"properties":{"name":"Eve"}}\n]'
+    assert result['data'] == expected_json
+    ''' EXAMPLE of JSON string:
+                [{"type":"node","id":"100","labels":["User"],"properties":{"name":"Eve"}}
+                ]
+    '''
+
+
+    # Now add relationship (with no properties) between the above two nodes
+    db.link_nodes_by_ids(node_id_eve, node_id_adam, "LOVES")
+    # Look up the Neo4j ID of the relationship just created
+    cypher = "MATCH (to)-[r]->(from) RETURN id(r) AS rel_id"
+    query_result = db.query(cypher)
+    rel_id_1 = query_result[0]["rel_id"]
+
+    result = db.export_nodes_rels_json()    # Look up everything: the 2 nodes and the 1 relationship
+    assert result['nodes'] == 2
+    assert result['relationships'] == 1
+    assert result['properties'] == 3
+    expected_json = f'[{{"type":"node","id":"{node_id_eve}","labels":["User"],"properties":{{"name":"Eve"}}}},\n' \
+                    f' {{"type":"node","id":"{node_id_adam}","labels":["User"],"properties":{{"name":"Adam","age":30}}}},\n' \
+                    f' {{"id":"{rel_id_1}","type":"relationship","label":"LOVES","start":{{"id":"{node_id_eve}","labels":["User"]}},"end":{{"id":"{node_id_adam}","labels":["User"]}}}}\n]'
+    assert result['data'] == expected_json
+    ''' EXAMPLE of JSON string:
+        [{"type":"node","id":"108","labels":["User"],"properties":{"name":"Eve"}},
+         {"type":"node","id":"109","labels":["User"],"properties":{"name":"Adam","age":30}},
+         {"id":"3","type":"relationship","label":"LOVES","start":{"id":"108","labels":["User"]},"end":{"id":"109","labels":["User"]}}
+        ]
+    '''
+
+    # Only retrieve the "Adam" node, plus all relationships
+    result = db.export_nodes_rels_json(nodes_query = "MATCH (n {name: 'Adam'})")    # Retrieves 1 node and 1 relationship
+    assert result['nodes'] == 1
+    assert result['relationships'] == 1
+    assert result['properties'] == 2    # The "name" and "age" for the `Adam` node
+    expected_json = f'[{{"type":"node","id":"{node_id_adam}","labels":["User"],"properties":{{"name":"Adam","age":30}}}},\n' \
+                    f' {{"id":"{rel_id_1}","type":"relationship","label":"LOVES","start":{{"id":"{node_id_eve}","labels":["User"]}},"end":{{"id":"{node_id_adam}","labels":["User"]}}}}\n]'
+    assert result['data'] == expected_json
+    ''' EXAMPLE of JSON string:
+        [{"type":"node","id":"109","labels":["User"],"properties":{"name":"Adam","age":30}},
+         {"id":"3","type":"relationship","label":"LOVES","start":{"id":"108","labels":["User"]},"end":{"id":"109","labels":["User"]}}
+        ]
+    '''
+
+    # Only retrieve the "Adam" node, and no relationships
+    result = db.export_nodes_rels_json( nodes_query = "MATCH (n {name: 'Adam'})",
+                                        rels_query = "MATCH ()-[r :NO_SUCH_NAME]->()")    # Retrieves 1 node and 0 relationships
+    assert result['nodes'] == 1
+    assert result['relationships'] == 0
+    assert result['properties'] == 2    # The "name" and "age" for the `Adam` node
+    expected_json = f'[{{"type":"node","id":"{node_id_adam}","labels":["User"],"properties":{{"name":"Adam","age":30}}}}\n]'
+    assert result['data'] == expected_json
+    ''' EXAMPLE of JSON string:
+        [{"type":"node","id":"109","labels":["User"],"properties":{"name":"Adam","age":30}}
+        ]
+    '''
+
+    # Only retrieve the relationship
+    result = db.export_nodes_rels_json(nodes_query = "MATCH (n :NON_EXISTENT_LABEL)")    # Only the relationship will be found
+    assert result['nodes'] == 0
+    assert result['relationships'] == 1
+    assert result['properties'] == 0
+
+    expected_json = f'[{{"id":"{rel_id_1}","type":"relationship","label":"LOVES","start":{{"id":"{node_id_eve}","labels":["User"]}},"end":{{"id":"{node_id_adam}","labels":["User"]}}}}\n]'
+    assert result['data'] == expected_json
+    ''' EXAMPLE of JSON string:
+        [{"id":"3","type":"relationship","label":"LOVES","start":{"id":"108","labels":["User"]},"end":{"id":"109","labels":["User"]}}
+        ]
+    '''
+
+    # Retrieve absolutely nothing
+    result = db.export_nodes_rels_json(nodes_query = "MATCH (n :NON_EXISTENT_LABEL)",
+                                        rels_query = "MATCH ()-[r :NO_SUCH_NAME]->()")    # Nothing will be found
+
+    assert result == {'nodes': 0, 'relationships': 0, 'properties': 0, 'data': '[\n]'}
+
+
+    # Add a 2nd relationship (this time with properties) between the two nodes
+    db.link_nodes_by_ids(node_id_eve, node_id_adam, "KNOWS", {'since': 1976, 'intensity': 'eternal'})
+    # Look up the Neo4j ID of the relationship just created
+    cypher = "MATCH (to)-[r:KNOWS]->(from) RETURN id(r) AS rel_id"
+    query_result = db.query(cypher)
+    rel_id_2 = query_result[0]["rel_id"]
+
+    result = db.export_nodes_rels_json()
+    assert result['nodes'] == 2
+    assert result['relationships'] == 2
+    assert result['properties'] == 5    # Note that the 2 properties in the latest relationship went into the count
+    expected_json = f'[{{"type":"node","id":"{node_id_eve}","labels":["User"],"properties":{{"name":"Eve"}}}},\n' \
+                    f' {{"type":"node","id":"{node_id_adam}","labels":["User"],"properties":{{"name":"Adam","age":30}}}},\n' \
+                    f' {{"id":"{rel_id_1}","type":"relationship","label":"LOVES","start":{{"id":"{node_id_eve}","labels":["User"]}},"end":{{"id":"{node_id_adam}","labels":["User"]}}}},\n' \
+                    f' {{"id":"{rel_id_2}","type":"relationship","label":"KNOWS","properties":{{"intensity":"eternal","since":1976}},"start":{{"id":"{node_id_eve}","labels":["User"]}},"end":{{"id":"{node_id_adam}","labels":["User"]}}}}\n]'
+    ''' EXAMPLE of JSON string:
+        [{"type":"node","id":"124","labels":["User"],"properties":{"name":"Eve"}},
+         {"type":"node","id":"125","labels":["User"],"properties":{"name":"Adam","age":30}},
+         {"id":"11","type":"relationship","label":"LOVES","start":{"id":"124","labels":["User"]},"end":{"id":"125","labels":["User"]}},
+         {"id":"12","type":"relationship","label":"KNOWS","properties":{"intensity":"eternal","since":1976},"start":{"id":"124","labels":["User"]},"end":{"id":"125","labels":["User"]}}
+        ]
+    '''
+
+    # The order of the relationships appears to vary randomly: also cover the reverse the lines for "LOVES" and "KNOWS"
+    # (TODO: it may be better to first parse the JSON string into a python entity and then assert)
+    expected_json_reversed_order = f'[{{"type":"node","id":"{node_id_eve}","labels":["User"],"properties":{{"name":"Eve"}}}},\n' \
+                    f' {{"type":"node","id":"{node_id_adam}","labels":["User"],"properties":{{"name":"Adam","age":30}}}},\n' \
+                    f' {{"id":"{rel_id_2}","type":"relationship","label":"KNOWS","properties":{{"intensity":"eternal","since":1976}},"start":{{"id":"{node_id_eve}","labels":["User"]}},"end":{{"id":"{node_id_adam}","labels":["User"]}}}},\n' \
+                    f' {{"id":"{rel_id_1}","type":"relationship","label":"LOVES","start":{{"id":"{node_id_eve}","labels":["User"]}},"end":{{"id":"{node_id_adam}","labels":["User"]}}}}\n]'
+
+    assert (result['data'] == expected_json) or (result['data'] == expected_json_reversed_order)
+
+
+    # Only request the "KNOWS" relationship
+    result = db.export_nodes_rels_json(rels_query = "MATCH ()-[r :KNOWS]->()")
+    assert result['nodes'] == 2
+    assert result['relationships'] == 1 # We ditched one of the two relationships
+    assert result['properties'] == 5
+    expected_json = f'[{{"type":"node","id":"{node_id_eve}","labels":["User"],"properties":{{"name":"Eve"}}}},\n' \
+                    f' {{"type":"node","id":"{node_id_adam}","labels":["User"],"properties":{{"name":"Adam","age":30}}}},\n' \
+                    f' {{"id":"{rel_id_2}","type":"relationship","label":"KNOWS","properties":{{"intensity":"eternal","since":1976}},"start":{{"id":"{node_id_eve}","labels":["User"]}},"end":{{"id":"{node_id_adam}","labels":["User"]}}}}\n]'
+    ''' EXAMPLE of JSON string:
+        [{"type":"node","id":"124","labels":["User"],"properties":{"name":"Eve"}},
+         {"type":"node","id":"125","labels":["User"],"properties":{"name":"Adam","age":30}},
+         {"id":"12","type":"relationship","label":"KNOWS","properties":{"intensity":"eternal","since":1976},"start":{"id":"124","labels":["User"]},"end":{"id":"125","labels":["User"]}}
+        ]
+    '''
+    assert result['data'] == expected_json
 
 
 
