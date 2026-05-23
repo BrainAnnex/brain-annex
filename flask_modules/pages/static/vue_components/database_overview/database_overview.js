1ecff563c20862e0a626b925b69ecda30a413be2
Julian West <41893046+BrainAnnex@users.noreply.github.com>
2026-05-23 13:15:36 -0700

    Continued early development of Vue.component 'vue-database-summary'

--- flask_modules/pages/static/vue_components/database_overview/database_overview.js
+++ flask_modules/pages/static/vue_components/database_overview/database_overview.js
@@ -11,22 +11,26 @@ Vue.component('vue-database-summary',
                 <h2>{{all_labels.length}} <i>labels</i> found in graph database:</h2>
 
                 <div v-for="(label, index) in all_labels" >
-                    Value: {{my_var}}<br>
 
-                    <button @click="my_var=true">CLICK ME</button>
+                    <span style="color: gray">{{String(index).padStart(2, '0')}} </span> *
 
-                    *
-                    <img @click="foo_schema(index)" class="clickable-icon"
+                    <img @click="toggle_schema(index)" class="clickable-icon"
                                 src="/BA/pages/static/graphics/info_16.png"
                                 title="Show info" alt="Show info">
                     &nbsp;
-                    <img @click="my_var=!my_var" class="clickable-icon"
+                    <img @click="toggle_sample(index)" class="clickable-icon"
                                 src="/BA/pages/static/graphics/tabular_16_9040670.png"
                                 title="Show sample node (record)" alt="Show sample node (record)">
+
+                    <span style="font-size: 14px">{{label}}</span>
                     <br>
 
                     <div v-if="show_schema_arr[index]" style="border: 1px solid gray; margin-bottom:10px">
-                        YO schema!
+                        Schema info here!
+                    </div>
+
+                    <div v-if="show_sample_arr[index]" style="border: 1px solid gray; margin-bottom:10px">
+                        Sample info here!
                     </div>
 
                 </div>
@@ -40,7 +44,7 @@ Vue.component('vue-database-summary',
         data: function() {
             return {
                 all_labels: this.all_database_labels,
-                my_var: false,
+
                 show_schema_arr: Array(this.all_database_labels.length).fill(false),
                 show_sample_arr: Array(this.all_database_labels.length).fill(false)
             }
@@ -51,11 +55,17 @@ Vue.component('vue-database-summary',
 
         // ------------------------------------   METHODS   ------------------------------------
         methods: {
-            foo_schema(index)
+            toggle_schema(index)
             {
                 console.log(index);
                 Vue.set(this.show_schema_arr, index, !this.show_schema_arr[index]);
-            }
+            },
+
+            toggle_sample(index)
+            {
+                console.log(index);
+                Vue.set(this.show_sample_arr, index, !this.show_sample_arr[index]);
+            },
 
         }  // METHODS
 
