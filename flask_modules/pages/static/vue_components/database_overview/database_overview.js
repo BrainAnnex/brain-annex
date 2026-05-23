69090234de594d3820da80384738e344441a50f9
Julian West <41893046+BrainAnnex@users.noreply.github.com>
2026-05-23 12:56:03 -0700

    Continued early development of Vue.component 'vue-database-summary'

--- flask_modules/pages/static/vue_components/database_overview/database_overview.js
+++ flask_modules/pages/static/vue_components/database_overview/database_overview.js
@@ -8,9 +8,28 @@ Vue.component('vue-database-summary',
         template: `
             <div>	<!-- Outer container, serving as Vue-required template root  -->
 
-                Value: {{my_var}}<br>
+                <h2>{{all_labels.length}} <i>labels</i> found in graph database:</h2>
 
-                <button @click="my_var=true">CLICK ME</button>
+                <div v-for="(label, index) in all_labels" >
+                    Value: {{my_var}}<br>
+
+                    <button @click="my_var=true">CLICK ME</button>
+
+                    *
+                    <img @click="foo_schema(index)" class="clickable-icon"
+                                src="/BA/pages/static/graphics/info_16.png"
+                                title="Show info" alt="Show info">
+                    &nbsp;
+                    <img @click="my_var=!my_var" class="clickable-icon"
+                                src="/BA/pages/static/graphics/tabular_16_9040670.png"
+                                title="Show sample node (record)" alt="Show sample node (record)">
+                    <br>
+
+                    <div v-if="show_schema_arr[index]" style="border: 1px solid gray; margin-bottom:10px">
+                        YO schema!
+                    </div>
+
+                </div>
 
             </div>		<!-- End of outer container -->
             `,
@@ -20,7 +39,10 @@ Vue.component('vue-database-summary',
         // ------------------------------------   DATA   ------------------------------------
         data: function() {
             return {
-                my_var: false
+                all_labels: this.all_database_labels,
+                my_var: false,
+                show_schema_arr: Array(this.all_database_labels.length).fill(false),
+                show_sample_arr: Array(this.all_database_labels.length).fill(false)
             }
         }, // data
 
@@ -29,7 +51,11 @@ Vue.component('vue-database-summary',
 
         // ------------------------------------   METHODS   ------------------------------------
         methods: {
-
+            foo_schema(index)
+            {
+                console.log(index);
+                Vue.set(this.show_schema_arr, index, !this.show_schema_arr[index]);
+            }
 
         }  // METHODS
 
